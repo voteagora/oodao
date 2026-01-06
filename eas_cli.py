e73db276ad814ef2c2cd64f936cdda5e29915873
Pedro Fernandes <pedrohasftb@gmail.com>
2026-01-06 16:35:08 -0300

    Optimism mainnet deployment

--- eas_cli.py
+++ eas_cli.py
@@ -20,11 +20,13 @@ load_dotenv()
 
 SCHEMA_CONTRACTS = {'11155111': '0x0a7E2Ff54e76B8E6659aedc9103FB21c038050D0',
                     '1' : '0xA7b39296258348C78294F95B872b282326A97BDF',
-                    '8453': '0x4200000000000000000000000000000000000020'}
+                    '8453': '0x4200000000000000000000000000000000000020',
+                    '10': '0x4200000000000000000000000000000000000020'}
 
 EAS_CONTRACTS = {'11155111': '0xC2679fBD37d54388Ce493F1DB75320D236e1815e',
                  '1': '0xA1207F3BBa224E2c9c3c6D5aF63D0eb1582Ce587',
-                 '8453': '0x4200000000000000000000000000000000000021'}
+                 '8453': '0x4200000000000000000000000000000000000021',
+                 '10': '0x4200000000000000000000000000000000000021'}
 
 # Client Generates the dao_id -> could be collissions, 
 # but as long as collissions are resolved by using the latest instantiate by a permissioned address... does it matter? Vanity addresses are possible here.
@@ -104,8 +106,8 @@ def get_deployment_config(chain_id):
         entity_resolver = '0x7106847Cc6c99E3D730D4f2a8312A905c0ad2ad7'
     elif chain_id == 10:
         rpc_url = 'https://optimism-rpc.publicnode.com'
-        votes_resolver = ...
-        entity_resolver = ...
+        votes_resolver = '0x3d0Ee8700f3A2267a677504FfEdAE54A15ABBE7B'
+        entity_resolver = '0x2829EE5e93cD1671140D8AE1fe7524Ba1F5AC6ad'
     elif chain_id == 11155420:
         rpc_url = 'https://sepolia.optimism.io'
         votes_resolver = ...
